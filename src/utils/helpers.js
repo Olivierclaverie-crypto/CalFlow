3f66cb7efc2f3509a7446736ebabf47614dadd92
Claverie <olivierclaverie@me.com>
2026-05-30 22:44:57 +0200

    Update helpers.js

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -15,6 +15,7 @@ export const todayISO = () => toISO(new Date());
 
 export function getWeekStart(date) {
   const d = new Date(date);
+  d.setHours(12, 0, 0, 0);
   const day = d.getDay();
   d.setDate(d.getDate() - ((day + 6) % 7));
   d.setHours(0, 0, 0, 0);
