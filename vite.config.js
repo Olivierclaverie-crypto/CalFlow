34d7ff851f558ee2aad7f1e77daabaf8eafdc142
Claverie <olivierclaverie@me.com>
2026-06-01 01:38:15 +0200

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,22 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
+import { readFileSync, writeFileSync } from "fs";
+
+function swVersionPlugin() {
+  return {
+    name: "sw-version",
+    buildStart() {
+      const swPath = "./public/sw.js";
+      let sw = readFileSync(swPath, "utf-8");
+      sw = sw.replace(/__TIMESTAMP__/, Date.now());
+      writeFileSync(swPath, sw);
+    },
+  };
+}
+
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), swVersionPlugin()],
   server: {
     proxy: {
       "/api": "http://localhost:3000",
